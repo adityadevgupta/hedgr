79de4d54fd3d30de33bd4033e07dcdc640f6f15f
Aditya Dev Gupta <adityadev93@gmail.com>
2015-03-23 18:08:00 -0700

    basic login flow

--- www/js/app.js
+++ www/js/app.js
@@ -1,83 +1,86 @@
-// Ionic Starter App
+// Ionic Parse Starter App
 
 // angular.module is a global place for creating, registering and retrieving Angular modules
-// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// 'ionicParseApp' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-// 'starter.services' is found in services.js
-// 'starter.controllers' is found in controllers.js
-angular.module('starter', ['ionic', 'starter.controllers', 'starter.services'])
+// 'ionicParseApp.controllers' is found in controllers.js
+angular.module('ionicParseApp',
+        [ 'ionic', 'ionicParseApp.controllers' ]
+    )
+    .config(function($stateProvider, $urlRouterProvider) {
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if (window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if (window.StatusBar) {
-      // org.apache.cordova.statusbar required
-      StatusBar.styleDefault();
-    }
-  });
-})
+        // Ionic uses AngularUI Router which uses the concept of states
+        // Learn more here: https://github.com/angular-ui/ui-router
+        // Set up the various states which the app can be in.
+        // Each state's controller can be found in controllers.js
+        $stateProvider
 
-.config(function($stateProvider, $urlRouterProvider) {
+            // setup an abstract state for the tabs directive
+            .state('welcome', {
+                url: '/welcome?clear',
+                templateUrl: 'templates/welcome.html',
+                controller: 'WelcomeController'
+            })
 
-  // Ionic uses AngularUI Router which uses the concept of states
-  // Learn more here: https://github.com/angular-ui/ui-router
-  // Set up the various states which the app can be in.
-  // Each state's controller can be found in controllers.js
-  $stateProvider
+            .state('app', {
+                url: '/app?clear',
+                abstract: true,
+                templateUrl: 'templates/menu.html',
+                controller: 'AppController'
+            })
 
-  // setup an abstract state for the tabs directive
-    .state('tab', {
-    url: "/tab",
-    abstract: true,
-    templateUrl: "templates/tabs.html"
-  })
+            .state('app.home', {
+                url: '/home',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/home.html',
+                        controller: 'HomeController'
+                    }
+                }
+            })
 
-  // Each tab has its own nav history stack:
+            .state('app.login', {
+                url: '/login',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/login.html',
+                        controller: 'LoginController'
+                    }
+                }
+            })
 
-  .state('tab.dash', {
-    url: '/dash',
-    views: {
-      'tab-dash': {
-        templateUrl: 'templates/tab-dash.html',
-        controller: 'DashCtrl'
-      }
-    }
-  })
+            .state('app.forgot', {
+                url: '/forgot',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/forgotPassword.html',
+                        controller: 'ForgotPasswordController'
+                    }
+                }
+            })
 
-  .state('tab.chats', {
-      url: '/chats',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/tab-chats.html',
-          controller: 'ChatsCtrl'
-        }
-      }
+            .state('app.register', {
+                url: '/register',
+                views: {
+                    'menuContent': {
+                        templateUrl: 'templates/register.html',
+                        controller: 'RegisterController'
+                    }
+                }
+            });
+
+        $urlRouterProvider.otherwise('/welcome');
     })
-    .state('tab.chat-detail', {
-      url: '/chats/:chatId',
-      views: {
-        'tab-chats': {
-          templateUrl: 'templates/chat-detail.html',
-          controller: 'ChatDetailCtrl'
+    .run(function ($state, $rootScope) {
+        Parse.initialize('nhxkv7FZXO47Vqcs3LMzV8jCKCoOJhP9MmGoa15a', 'ZvE4DaGgbwJMSrrf05UEZmafTOSgBqdfl5jRmvZX');
+        var currentUser = Parse.User.current();
+        $rootScope.user = null;
+        $rootScope.isLoggedIn = false;
+
+        if (currentUser) {
+            $rootScope.user = currentUser;
+            $rootScope.isLoggedIn = true;
+            $state.go('app.home');
         }
-      }
-    })
+    });
 
-  .state('tab.account', {
-    url: '/account',
-    views: {
-      'tab-account': {
-        templateUrl: 'templates/tab-account.html',
-        controller: 'AccountCtrl'
-      }
-    }
-  });
-
-  // if none of the above states are matched, use this as the fallback
-  $urlRouterProvider.otherwise('/tab/dash');
-
-});
